54ab710df0b36b6dfbdc1f2f54882f53ba056738
baronix <baronwork@icloud.com>
2023-10-08 03:11:42 +0100

    dx

---
+++ src/App.spec.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.createRoot(div).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
