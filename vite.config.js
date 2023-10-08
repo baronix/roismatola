95c0140fd8b616b7cab1a7779239cfb296443e21
baronix <baronwork@icloud.com>
2023-10-08 05:36:00 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {alias:{"@": "/src"}},
+  // resolve: {alias:{"@": "/src"}},
   plugins: [react()],
   server: {
     host: true,
