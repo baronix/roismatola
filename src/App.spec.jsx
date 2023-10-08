341de443972a57ef7f7279b9aeb4c6e1e6b2fbd2
baronix <baronwork@icloud.com>
2023-10-08 03:28:24 +0100

    Update App.spec.jsx

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,6 +1,7 @@
 import {React, StrictMode, createElement} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import createRoot from "react-dom/client";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
