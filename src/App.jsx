1a995235f99de4aca2cda4f5d190f6a0170b45e2
baronix <baronwork@icloud.com>
2023-10-08 12:50:14 +0100

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import React, { useState, createElement } from 'react';
-import './App.css';
+// import './App.css';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-     
-      <h1>Alice</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ResponsiveAppBar/>
     </div>
   )
 }
